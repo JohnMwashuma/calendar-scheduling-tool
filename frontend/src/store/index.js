dde8bf23e0625cbf28006cffbe38ebfe55faa3de
JOHNMWASHUMA <jmwashuma@live.com>
2025-05-11 03:10:13 +0300

    Set up redux store, reducers and actions

---
+++ frontend/src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import authReducer from './reducers/authReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export default store;
